0f3c417e7eda2e03858afb8b02f14725b5c4a96e
Md Meraj <08merajshaikh@gmail.com>
2026-08-11 12:12:42 +0530

    Time: 0 ms (100%), Space: 21 MB (35.08%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,65 +1,69 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<int>row;
-        vector<int>col;
+        int c0=1;
         int m=matrix.size();
         int n=matrix[0].size();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    row.push_back(i);
-                    col.push_back(j);
+                    matrix[i][0]=0;
+                    if(j==0)c0=0;
+                    else{
+                        matrix[0][j]=0;
+                    }
                 }
             }
         }
-        for(int i=0;i<row.size();i++){
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0)matrix[i][j]=0;
+            }
+        }
+        if(matrix[0][0]==0){
             for(int j=0;j<n;j++){
-                matrix[row[i]][j]=0;
+                matrix[0][j]=0;
             }
         }
-        for(int j=0;j<col.size();j++){
+        if(c0==0){
             for(int i=0;i<m;i++){
-                matrix[i][col[j]]=0;
+                matrix[i][0]=0;
             }
         }
-
     }
 };
 
 
+
+
+
+
+// another method having time and space complexity is o(n2)
 // class Solution {
 // public:
 //     void setZeroes(vector<vector<int>>& matrix) {
+//         vector<int>row;
+//         vector<int>col;
 //         int m=matrix.size();
 //         int n=matrix[0].size();
-//         vector<int>index;
-//         vector<int>index2;
-
 //         for(int i=0;i<m;i++){
 //             for(int j=0;j<n;j++){
 //                 if(matrix[i][j]==0){
-//                   index.push_back(i);
-//                   index2.push_back(j);
-                  
+//                     row.push_back(i);
+//                     col.push_back(j);
 //                 }
 //             }
 //         }
-//         int n1=index.size();
-        
-         
-//         for(int i=0;i<n1;i++){
+//         for(int i=0;i<row.size();i++){
 //             for(int j=0;j<n;j++){
-//                 matrix[index[i]][j]=0;
+//                 matrix[row[i]][j]=0;
 //             }
 //         }
-//         int n2=index2.size();
-//         for(int j=0;j<n2;j++){
+//         for(int j=0;j<col.size();j++){
 //             for(int i=0;i<m;i++){
-//                 matrix[i][index2[j]]=0;
+//                 matrix[i][col[j]]=0;
 //             }
 //         }
-        
 
 //     }
 // };
