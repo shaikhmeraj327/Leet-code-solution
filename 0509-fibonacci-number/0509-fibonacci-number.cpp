268b5efe3614236387093c3036858715d3bd2371
Md Meraj <08merajshaikh@gmail.com>
2026-09-03 20:21:08 +0530

    Time: 0 ms (100%), Space: 7.8 MB (38.66%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,13 +1,29 @@
 class Solution {
-    public:
+public:
     int fib(int n) {
         if(n<=1)return n;
-       vector<int>dp(n+1);
-       dp[0]=0;
-       dp[1]=1;
-       for(int i=2;i<=n;i++){
-        dp[i]=dp[i-1]+dp[i-2];
-       } 
-       return dp[n];
+        int prev1=1;
+        int prev2=0;
+        for(int i=2;i<=n;i++){
+            int curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
     }
 };
+
+
+// class Solution {
+//     public:
+//     int fib(int n) {
+//         if(n<=1)return n;
+//        vector<int>dp(n+1);
+//        dp[0]=0;
+//        dp[1]=1;
+//        for(int i=2;i<=n;i++){
+//         dp[i]=dp[i-1]+dp[i-2];
+//        } 
+//        return dp[n];
+//     }
+// };
