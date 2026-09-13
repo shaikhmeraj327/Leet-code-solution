995859c20aca8f3457367ead32abd2701d661eb0
Md Meraj <08merajshaikh@gmail.com>
2026-09-13 12:15:18 +0530

    Time: 8 ms (47.74%), Space: 12.4 MB (55.74%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -2,18 +2,39 @@ class Solution {
 public:
     int maxSatisfaction(vector<int>& satisfaction) {
         int n=satisfaction.size();
-        sort(satisfaction.begin(), satisfaction.end());
-        vector<vector<int>>dp(n+1,vector<int>(n+2,0));
+        sort(satisfaction.begin(),satisfaction.end());
+        vector<int>curr(n+1,0);
+        vector<int>next(n+1,0);
         for(int ind=n-1;ind>=0;ind--){
-            for(int time=n;time>=1;time--){
-                int include=time*satisfaction[ind]+dp[ind+1][time+1];
-                int exclude=0+dp[ind+1][time];
-                dp[ind][time]=max(include,exclude);
+            for(int time=ind;time>=0;time--){
+                int include=(time+1)*satisfaction[ind]+next[time+1];
+                int exclude=0+next[time];
+                curr[time]=max(include,exclude);
             }
+            next=curr;
         }
-        return dp[0][1];
+        return next[0];
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int maxSatisfaction(vector<int>& satisfaction) {
+//         int n=satisfaction.size();
+//         sort(satisfaction.begin(), satisfaction.end());
+//         vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+//         for(int ind=n-1;ind>=0;ind--){
+//             for(int time=ind;time>=0;time--){
+//                 int include=(time+1)*satisfaction[ind]+dp[ind+1][time+1];
+//                 int exclude=0+dp[ind+1][time];
+//                 dp[ind][time]=max(include,exclude);
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// };
 
 
 
