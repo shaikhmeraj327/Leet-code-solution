cef2502044dbc4946d4a2bf9149b61650b2269dc
Md Meraj <08merajshaikh@gmail.com>
2026-09-13 13:34:56 +0530

    Time: 167 ms (37.17%), Space: 14.6 MB (40.8%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,20 +2,45 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        vector<int>currRow(n+1,0);
+        vector<int>prevRow(n+1,0);
         for(int curr=n-1;curr>=0;curr--){
-            for(int prev=curr-1;prev>=-1;prev--){
+            for(int prev=curr;prev>=-1;prev--){
                 int include=0;
                 if(prev==-1 || nums[curr]>nums[prev]){
-                    include=1+dp[curr+1][curr+1];
-                }
-                int exclude=0+dp[curr+1][prev+1];
-                dp[curr][prev+1]=max(include,exclude);
+                    include=1+currRow[curr+1];
+                } 
+                int exclude=0+currRow[prev+1];
+                prevRow[prev+1]=max(include,exclude);
             }
+            currRow=prevRow;
         }
-        return dp[0][0];
+        return currRow[0];
     }
 };
+
+
+        
+
+
+// class Solution {
+// public:
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+//         for(int curr=n-1;curr>=0;curr--){
+//             for(int prev=curr-1;prev>=-1;prev--){
+//                 int include=0;
+//                 if(prev==-1 || nums[curr]>nums[prev]){
+//                     include=1+dp[curr+1][curr+1];
+//                 }
+//                 int exclude=0+dp[curr+1][prev+1];
+//                 dp[curr][prev+1]=max(include,exclude);
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// };
 
 
 
@@ -40,4 +65,4 @@ public:
 //         vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
 //         return solve(nums,prev,0,n,dp);
 //     }
-// };
+// }; 
